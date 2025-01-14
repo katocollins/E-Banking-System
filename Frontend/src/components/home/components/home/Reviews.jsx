b0e62a1eb06cf72f99686d8dbe3e805185438ec3
Kato Collins <108077573+katocollins@users.noreply.github.com>
2025-01-14 10:34:27 +0300

    Update Reviews.jsx

--- Frontend/src/components/home/components/home/Reviews.jsx
+++ Frontend/src/components/home/components/home/Reviews.jsx
@@ -103,50 +103,6 @@ export default function Reviews() {
             <div className="relative flex items-start border-x-4 border-blue-800 rounded shadow">
               {/* Testimonial */}
               <div className="text-center md:px-12 py-20 md:pt-20 mx-4 md:mx-0">
-                <div className="absolute top-0 -mt-8 left-1/2 transform -translate-x-1/2">
-                  <svg
-                    className="absolute top-0 right-0 -mt-3 -mr-8 w-16 h-16 fill-current text-blue-800"
-                    viewBox="0 0 64 64"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M37.89 58.338c-2.648-5.63-3.572-10.045-2.774-13.249.8-3.203 8.711-13.383 23.737-30.538l2.135.532c-6.552 10.033-10.532 17.87-11.939 23.515-.583 2.34.22 6.158 2.41 11.457l-13.57 8.283zm-26.963-6.56c-2.648-5.63-3.572-10.046-2.773-13.25.799-3.203 8.71-13.382 23.736-30.538l2.136.533c-6.552 10.032-10.532 17.87-11.94 23.515-.583 2.339.22 6.158 2.41 11.456l-13.57 8.283z" />
-                  </svg>
-                  <img
-                    className="relative rounded-full"
-                    src={robertPhoto}
-                    width="96"
-                    height="96"
-                    alt="Robert Brown"
-                  />
-                </div>
-
-                <blockquote className="text-xl font-medium mb-4 flex items-center justify-center">
-                  {" "}
-                  <span className="text-orange-500">
-                    {" "}
-                    <FaQuoteLeft size={30} className="mr-2" />{" "}
-                  </span>{" "}
-                  E-Bank has made banking so easy for me! The multiple account
-                  feature is wonderful, and I can transfer, withdraw, and
-                  deposit money anywhere and anytime. The support service is
-                  also top-notch, always ready to assist with any questions or
-                  concerns. E-Bank has truly revolutionized the way I manage my
-                  finances.{" "}
-                  <span className="text-orange-500">
-                    {" "}
-                    <FaQuoteRight size={30} className="ml-2" />{" "}
-                  </span>{" "}
-                </blockquote>
-                <cite className="block font-bold text-lg not-italic mb-1">
-                  Robert Brown
-                </cite>
-                <div className="text-gray-600">
-                  <span>CEO & Co-Founder</span>{" "}
-                  <a className="text-blue-600 hover:underline" href="#0">
-                    @FakeCO
-                  </a>
-                </div>
               </div>
             </div>
             <div className="flex justify-center items-center my-5">
